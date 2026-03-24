07e53183541bf635befb1c800baacc0017a7d3fb
Athul3588krishna <athul98517772@gmail.com>
2026-03-24 23:16:14 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Backend set ann tto");
+});
+
+// connect DB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
+
+
+
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
